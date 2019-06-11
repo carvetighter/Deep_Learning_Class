3e48e3f5b680a8223c00842c4ab49c2aa76daee9
Brent Allen <carvetighter@gmail.com>
2019-06-10 19:24:35 -0500

    completed feature scaling for rnn

--- rnn/bka_rnn_00.py
+++ rnn/bka_rnn_00.py
@@ -22,6 +22,7 @@ pip install --upgrade keras
 import numpy
 from matplotlib import pyplot
 import pandas
+from sklearn.preprocessing import MinMaxScaler
 
 #---------------------------------------------------------------------------------------------#
 # keras imports
@@ -31,6 +32,61 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#
+# Feature Engineering
+#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#---------------------------------------------------------------------------------------------#
+# Train
+#---------------------------------------------------------------------------------------------#
+
+# Importing the training set
+dataset_train = pandas.read_csv('Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = numpy.array(X_train), numpy.array(y_train)
+
+# Reshaping
+X_train = numpy.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+#---------------------------------------------------------------------------------------------#
+# Test
+#---------------------------------------------------------------------------------------------#
+
+# Getting the real stock price of 2017
+dataset_test = pandas.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting the predicted stock price of 2017
+# Feature Scaling
+dataset_total = pandas.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = numpy.array(X_test)
+
+# reshape
+X_test = numpy.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
