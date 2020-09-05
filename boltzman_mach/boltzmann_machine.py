2d0b6d5adf3c4ebb4539e19ff67df8534e1d92f2
Brent Allen <carvetighter@gmail.com>
2020-09-05 17:44:19 -0500

    updating code

--- boltzman_mach/boltzmann_machine.py
+++ boltzman_mach/boltzmann_machine.py
@@ -37,19 +37,34 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
+import os
 
 """## Importing the dataset"""
 
+# base path
+string_path_base = os.path.abspath('.')
+string_path_boltz = os.path.join(string_path_base, 'boltzman_mach')
+
 # We won't be using this dataset.
-movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
-ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+'''
+movies = pd.read_csv(
+    'ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1'
+)
+users = pd.read_csv(
+    'ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1'
+)
+ratings = pd.read_csv(
+    'ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1'
+)
+'''
 
 """## Preparing the training set and the test set"""
 
-training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+string_pf_train = os.path.join(string_path_boltz, 'ml-100k/u1.base')
+string_pf_test = os.path.join(string_path_boltz, 'ml-100k/u1.test')
+training_set = pd.read_csv(string_pf_train, delimiter = '\t')
 training_set = np.array(training_set, dtype = 'int')
-test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = pd.read_csv(string_pf_test, delimiter = '\t')
 test_set = np.array(test_set, dtype = 'int')
 
 """## Getting the number of users and movies"""
@@ -60,14 +75,14 @@ nb_movies = int(max(max(training_set[:, 1], ), max(test_set[:, 1])))
 """## Converting the data into an array with users in lines and movies in columns"""
 
 def convert(data):
-  new_data = []
-  for id_users in range(1, nb_users + 1):
-    id_movies = data[:, 1] [data[:, 0] == id_users]
-    id_ratings = data[:, 2] [data[:, 0] == id_users]
-    ratings = np.zeros(nb_movies)
-    ratings[id_movies - 1] = id_ratings
-    new_data.append(list(ratings))
-  return new_data
+    new_data = []
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:, 1] [data[:, 0] == id_users]
+        id_ratings = data[:, 2] [data[:, 0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
 training_set = convert(training_set)
 test_set = convert(test_set)
 
@@ -90,24 +105,24 @@ test_set[test_set >= 3] = 1
 """## Creating the architecture of the Neural Network"""
 
 class RBM():
-  def __init__(self, nv, nh):
-    self.W = torch.randn(nh, nv)
-    self.a = torch.randn(1, nh)
-    self.b = torch.randn(1, nv)
-  def sample_h(self, x):
-    wx = torch.mm(x, self.W.t())
-    activation = wx + self.a.expand_as(wx)
-    p_h_given_v = torch.sigmoid(activation)
-    return p_h_given_v, torch.bernoulli(p_h_given_v)
-  def sample_v(self, y):
-    wy = torch.mm(y, self.W)
-    activation = wy + self.b.expand_as(wy)
-    p_v_given_h = torch.sigmoid(activation)
-    return p_v_given_h, torch.bernoulli(p_v_given_h)
-  def train(self, v0, vk, ph0, phk):
-    self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
-    self.b += torch.sum((v0 - vk), 0)
-    self.a += torch.sum((ph0 - phk), 0)
+    def __init__(self, nv, nh):
+        self.W = torch.randn(nh, nv)
+        self.a = torch.randn(1, nh)
+        self.b = torch.randn(1, nv)
+    def sample_h(self, x):
+        wx = torch.mm(x, self.W.t())
+        activation = wx + self.a.expand_as(wx)
+        p_h_given_v = torch.sigmoid(activation)
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
+    def sample_v(self, y):
+        wy = torch.mm(y, self.W)
+        activation = wy + self.b.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
+    def train(self, v0, vk, ph0, phk):
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+        self.b += torch.sum((v0 - vk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
 nv = len(training_set[0])
 nh = 100
 batch_size = 100
@@ -117,21 +132,21 @@ rbm = RBM(nv, nh)
 
 nb_epoch = 10
 for epoch in range(1, nb_epoch + 1):
-  train_loss = 0
-  s = 0.
-  for id_user in range(0, nb_users - batch_size, batch_size):
-    vk = training_set[id_user : id_user + batch_size]
-    v0 = training_set[id_user : id_user + batch_size]
-    ph0,_ = rbm.sample_h(v0)
-    for k in range(10):
-      _,hk = rbm.sample_h(vk)
-      _,vk = rbm.sample_v(hk)
-      vk[v0<0] = v0[v0<0]
+    train_loss = 0
+    s = 0.
+    for id_user in range(0, nb_users - batch_size, batch_size):
+        vk = training_set[id_user : id_user + batch_size]
+        v0 = training_set[id_user : id_user + batch_size]
+        ph0,_ = rbm.sample_h(v0)
+        for k in range(10):
+            _,hk = rbm.sample_h(vk)
+            _,vk = rbm.sample_v(hk)
+            vk[v0<0] = v0[v0<0]
     phk,_ = rbm.sample_h(vk)
     rbm.train(v0, vk, ph0, phk)
     train_loss += torch.mean(torch.abs(v0[v0 >= 0] - vk[v0 >= 0]))
     s += 1.
-  print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
+    print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
 
 """## Testing the RBM"""
 
